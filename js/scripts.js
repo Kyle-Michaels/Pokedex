0d7278c8d253a0134e454522bc29195f2afe85a5
Kyle Michaels <kyle.michaels@gmail.com>
2023-11-13 20:13:38 -0800

    Added Bulbasaur family to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
-alert('Hello world');
-
-let favoriteFood = 'Oran Berry';
-document.write(favoriteFood);
+let  pokemonList = [
+    {
+        id: 1,
+        name: 'Bulbasaur',
+        height: '0.7 m',
+        type: ['grass', 'poison'],
+    },
+    {
+        id: 2,
+        name: 'Ivysaur',
+        height: '1 m',
+        type: ['grass', 'poison'],
+    },
+    {
+        id: 3,
+        name: 'Venusaur',
+        height: '2 m',
+        type: ['grass', 'poison'],
+    },
+];
