a77868404f1ec08890b251636cc0c1aa6aa2820f
Kyle Michaels <kyle.michaels@gmail.com>
2023-11-23 12:24:19 -0800

    removed hardcoded pokemon repository. added api url

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,6 @@
 let pokemonRepository = (function () {
-    let  pokemonList = [
-        {
-            id: 1,
-            name: 'Bulbasaur',
-            height: 0.7,
-            type: ['grass', 'poison'],
-        },
-        {
-            id: 2,
-            name: 'Ivysaur',
-            height: 1,
-            type: ['grass', 'poison'],
-        },
-        {
-            id: 3,
-            name: 'Venusaur',
-            height: 2,
-            type: ['grass', 'poison'],
-        },
-        {
-            id: 4,
-            name: 'Charmander',
-            height: 0.6,
-            type: ['fire'],
-        },
-        {
-            id: 5,
-            name: 'Charmeleon',
-            height: 1.1,
-            type: ['fire'],
-        },
-        {
-            id: 6,
-            name: 'Charizard',
-            height: 1.7,
-            type: ['fire', 'flying'],
-        },
-        {
-            id: 7,
-            name: 'Squirtle',
-            height: 0.5,
-            type: ['water'],
-        },
-        {
-            id: 8,
-            name: 'Wartortle',
-            height: 1,
-            type: ['water'],
-        },
-        {
-            id: 9,
-            name: 'Blastoise',
-            height: 1.6,
-            type: ['water'],
-        },
-    ];
+    let  pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function getAll() {
         return pokemonList;
@@ -101,6 +47,8 @@ let pokemonRepository = (function () {
             showDetails(pokemon);
         })
     }
+
+
 
     return {
         getAll: getAll,
